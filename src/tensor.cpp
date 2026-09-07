9b10f3a51ff5531268c89719c9602f8d1ca4eccb
Shrey Birmiwal <shreybirmiwal@gmail.com>
2026-09-07 14:00:16 -0500

    skelleton for mult

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -23,6 +23,41 @@ Tensor::Tensor(vector<int> dims, vector<float> passed_data)
 
 Tensor Tensor::multiply(Tensor other)
 {
+
+    // x,y,z @ x,y,z
+    // x must match
+    // z1 must match y2
+
+    // output dims x, y1, z2
+    Tensor output;
+
+    // check dims
+    if (dimension_sizes.size() == 3)
+    {
+        if (other.dimension_sizes.size() != 3 || other.dimension_sizes[0] != dimension_sizes[0])
+        {
+            throw std::invalid_argument("dims dont match");
+        }
+
+        if (dimension_sizes[2] != other.dimension_sizes[1])
+        {
+            throw std::invalid_argument("dims dont match");
+        }
+
+        output = new Tensor(new vector<3, dimension_sizes[1], other.dimension_sizes[2]>, malloc(size_of(float) * 3 * dimension_sizes[1] * other.dimension_sizes[2]));
+    }
+    // must be 2d
+    else
+    {
+        if (dimension_sizes[1] != other.dimension_sizes[0])
+        {
+            throw std::invalid_argument("dims dont match");
+        }
+
+        output = new Tensor(new vector<2, dimension_sizes[1], other.dimension_sizes[0]>, malloc(size_of(float) * 3 * dimension_sizes[1] * other.dimension_sizes[0]));
+    }
+
+    // do actual multiplication
+
     return other;
-    // check dims!
 }
